2ba63d2f2675b5085313e6f4d2003b1d3b8b740e
Trần Hoài Nam <namth0943@ut.edu.vn>
2025-07-02 00:06:27 +0700

    Delete gender-healthcare-service-management directory

--- gender-healthcare-service-management/src/main/java/com/example/genderhealthcare/repository/ConsultantRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.genderhealthcare.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.genderhealthcare.entity.Consultant;
-
-public interface ConsultantRepository extends JpaRepository<Consultant, Long> {
-}
--- gender-healthcare-service-management/src/main/java/com/example/genderhealthcare/repository/ServiceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.genderhealthcare.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.genderhealthcare.entity.Service;
-
-public interface ServiceRepository extends JpaRepository<Service, Long> {
-}
