ccfaf94a44581374a72530ae75ec27a00b015824
Quocduy999 <quocduy74525@gmail.com>
2025-07-03 03:33:44 +0700

    update code lai

--- src/main/resources/static/js/questions.js
+++ src/main/resources/static/js/questions.js
@@ -156,3 +156,11 @@ document.addEventListener("DOMContentLoaded", () => {
   loadPatientName();
   loadQuestions();
 });
+document.addEventListener("DOMContentLoaded", () => {
+    const path = window.location.pathname;
+    document.querySelectorAll(".nav-link").forEach(link => {
+      if (link.getAttribute("href") === path) {
+        link.classList.add("active");
+      }
+    });
+  });
